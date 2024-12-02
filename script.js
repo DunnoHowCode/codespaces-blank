602a5d32621c7fd4d69895de90f367b544fce729
DunnoHowCode <ljsmith9@cps.edu>
2024-12-02 17:27:05 +0000

    pt2

--- script.js
+++ script.js
@@ -1,31 +1,70 @@
 left = document.querySelector('.lel');
 right = document.querySelector('.rofl');
-path = document.querySelector('.path');
+dolo = document.querySelector('.dolo');
 text = document.querySelector('p');
 dysto = document.querySelector('.dysto');
-plains = document.querySelector('.plains');
-prison = document.querySelector('.prison')
-path.style.display="none";
-plains.style.display="none";
+minecraft = document.querySelector('.minecraft');
+prison = document.querySelector('.prison');
+block = document.querySelector('.block');
+groupie = document.querySelector('.groupie');
+escape = document.querySelector('.outtie');
+injured = document.querySelector('.injured')
+
+block.style.display="none";
+dolo.style.display="none";
+minecraft.style.display="none";
 prison.style.display="none";
+groupie.style.display="none";
+escape.style.display="none";
+injured.style.display="none";
 
 left.addEventListener ('click', function() {
     left.style.display="none";
     right.style.display="none";
     dysto.style.display="none";
-    path.style.display="block";
-    text.innerHTML="Instead of continuing to live with chains dragging you down, you decide to take on the sole ambition of freedom. The task is nearly impossible alone, but the thought of someone dying for you is a deterrent, what do you do? <br><br> DBL Click to go alone, or search the screen to get a team anyway.";
+    dolo.style.display="inline";
+    groupie.style.display="inline";
+    text.innerHTML="Instead of continuing to live with chains dragging you down, you decide to take on the sole ambition of freedom. The task is nearly impossible alone, but the thought of someone dying for you is a deterrent, what do you do? <br><br> DBL Click to go alone, or hover over the image to get a team anyway.";
 });
 
 
-path.addEventListener ('dblclick', function() {
+dolo.addEventListener ('dblclick', function() {
     text.innerHTML="Being solo, it was easier to hide as you progressed. However you couldn't hide for long, nor could you cover the bases needed to make it out with your limited skill set. You were found and imprisoned for your attempt to escape.";
-    path.style.display="none";
+    dolo.style.display="none";
+    groupie.style.display="none";
     prison.style.display="block";
+    
 });
 
-path.addEventListener ('mouseenter', function() {
-    text.innerHTML="Where in the—";
-    path.style.display="none";
-    prison.style.display="block";
+groupie.addEventListener ('mouseenter', function() {
+    text.innerHTML="You made it out! Somehow.";
+    dolo.style.display="none";
+    groupie.style.display="none";
+    escape.style.display="block";
+    
 });
+
+right.addEventListener ('click', function() {
+    left.style.display="none";
+    right.style.display="none";
+    dysto.style.display="none";
+    injured.style.display="block";
+    text.innerHTML="The only feasible option in your mind is to try to make it through this ragged world one day at a time. We can leave the courageous ideals to courageous people. Still, that stale, unchanging suffering wouldn’t last long as you’d stumble upon one of the well-known leaders of this skewed world in terrible condition. In his hand held a weird looking block of grass. <br><br> Press W to pick him up, or (do smth) to take the block.";
+});
+
+document.addEventListener ('keydown', function(event) {
+    //87 = W, press W
+    if(event.keyCode == 87) {
+        injured.style.display="none";
+    }
+
+
+});
+
+block.addEventListener ('click', function() {
+    text.innerHTML="Where in the—";
+    injured.style.display="none";
+    minecraft.style.display="block";
+    block.style.display="none";
+});
+
